1c4369c6b0fc9228b0cffb0ecbce2ba97dfd1f64
Shadowlord69 <keithchow101@yahoo.com>
2020-05-02 20:21:09 -0400

    initial board design

--- connect4.js
+++ connect4.js
@@ -1,5 +1,23 @@
 class Connect4 {
   constructor(selector) {
-    this.row
+    this.NUMROWS = 6;
+    this.NUMCOLS = 7;
+    this.selector = selector;
+    this.createGrid();
+  }
+
+  createGrid() {
+    const $board = $(this.selector);
+    console.log($board);
+    for (let row = 0; row < this.NUMROWS; row++) {
+      const $row = $('<div>')
+        .addClass('row');
+      $board.append($row);
+      for (let col = 0; col < this.NUMCOLS; col++) {
+        const $col = $('<div>')
+          .addClass('col empty');
+        $row.append($col);
+      }
+    }
   }
 }
