3d490ce2ac8b140b58e37d2f700e61f363ecef8b
Keith Chow <keithchow101@yahoo.com>
2020-05-05 15:17:53 -0400

    Revert "changed board to be cols of rows from rows of cols"
    This reverts commit 3329b0fc39d6211c35779c5cb7fd13eba0e11cd5.

--- connect4.js
+++ connect4.js
@@ -4,26 +4,20 @@ class Connect4 {
     this.NUMCOLS = 7;
     this.selector = selector;
     this.createGrid();
-    this.setupEventListeners();
   }
 
   createGrid() {
     const $board = $(this.selector);
-    for (let col = 0; col < this.NUMCOLS; col++) {
-      const $col = $('<div>')
-        .addClass('col');
-      for (let row = 0; row < this.NUMROWS; row++) {
-        const $row = $('<div>')
-          .addClass('row empty');
-        $col.append($row);
+    console.log($board);
+    for (let row = 0; row < this.NUMROWS; row++) {
+      const $row = $('<div>')
+        .addClass('row');
+      $board.append($row);
+      for (let col = 0; col < this.NUMCOLS; col++) {
+        const $col = $('<div>')
+          .addClass('col empty');
+        $row.append($col);
       }
-      $board.append($col);
     }
   }
-  setupEventListeners(){
-    const $board = $(this.selector);
-    $board.on('mouseenter', '.row.empty', function(){
-      console.log('here', this);
-    })
-  } 
 }
