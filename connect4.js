a4d2f1aecc7d8cb2f28d1e986e7bb45ae168597c
Keith Chow <keithchow101@yahoo.com>
2020-05-05 17:55:24 -0400

    hovering over columns

--- connect4.js
+++ connect4.js
@@ -4,20 +4,46 @@ class Connect4 {
     this.NUMCOLS = 7;
     this.selector = selector;
     this.createGrid();
+    this.setupEventListeners();
   }
 
   createGrid() {
     const $board = $(this.selector);
-    console.log($board);
     for (let row = 0; row < this.NUMROWS; row++) {
       const $row = $('<div>')
         .addClass('row');
-      $board.append($row);
       for (let col = 0; col < this.NUMCOLS; col++) {
         const $col = $('<div>')
-          .addClass('col empty');
+          .addClass('col empty')
+          .attr('data-col-idx', col)
+          .attr('data-row-idx', row);
         $row.append($col);
       }
+      $board.append($row);
     }
+  }
+  setupEventListeners(){
+    const $board = $(this.selector);
+
+    function findLastEmptyCell(col) {
+      const cells = $(`.col[data-col-idx='${col}']`);
+      for (let i = cells.length - 1; i > -1; i--) {
+        const $cell = $(cells[i]);
+        if ($cell.hasClass('empty')) {
+          return $cell;
+        }
+      }
+      return null;
+    }
+
+    $board.on('mouseenter', '.col.empty', function() {
+      const col = $(this).data('col-idx');
+      const $lastEmptyCell = findLastEmptyCell(col);
+      $lastEmptyCell.addClass(`next-red`);
+    })
+
+    $board.on('mouseleave', '.col', function() {
+      $('.col').removeClass(`next-red`);
+    })
   }
 }
